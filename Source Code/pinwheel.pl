0703fff49b0741113e14d74cab64d457399cfd76
Mosaal <alexandremoreira_13@sapo.pt>
2015-12-10 20:33:10 +0000

    Quase feito
    Falta interface

--- Source Code/pinwheel.pl
+++ Source Code/pinwheel.pl
@@ -1,139 +1,63 @@
-:- use_module(library(aggregate)).
-:- use_module(library(between)).
-:- use_module(library(random)).
 :- use_module(library(lists)).
 :- use_module(library(clpfd)).
+:- use_module(library(random)).
 
-% Board
-outerWheel([3, 3, 4, 4, 5, 5, 6, 7, 8]).
-innerWheel([4, 4, 4, 5, 5, 5, 6, 6, 6]).
-innerWheel([2, 3, 4, 5, 5, 6, 7, 7, 8]).
-innerWheel([1, 2, 4, 4, 5, 6, 6, 7, 8]).
+% Aux. Variables
+:- dynamic randomSolution/1.
+randomSolution(0).
 
 % Interface
-valid_input(Input) :-
-	Input = e, !;
-	Input = h, !;
-	fail.
-
-choose_difficulty(Input) :-
-	write('Choose difficulty:\n'),
-	write(' - Easy (e)\n'),
-	write(' - Hard (h)\n'),
-	read(Input),
-	valid_input(Input), !;
-	choose_difficulty(Input).
-
-display_inner_wheels([]).
-display_inner_wheels([H|T]) :-
+display_board([]).
+display_board([H|T]) :-
 	print(H), nl,
-	display_inner_wheels(T).
+	display_board(T).
 
-display_all_wheels([]).
-display_all_wheels([H|T]) :-
-	print(H), nl,
-	display_all_wheels(T).
+reset_timer :- statistics(walltime, _).
+print_time :-
+	statistics(walltime, [_, T]),
+	TS is ((T // 10) * 10) / 1000,
+	nl, write('Time: '), write(TS), write('s'), nl.
 
 % Logic
-restrict_equal_wheel(Wheel, OuterWheel, Same, Same) :-
-	nth0(Same, Wheel, WheelElem),
-	nth0(Same, OuterWheel, OuterWheelElem),
-	OuterWheelElem #= WheelElem.
+create_matrix(0, _, _).
+create_matrix(Rows, Columns, Matrix) :-
+	NewRows is Rows - 1,
+	length(InsideRow, Columns),
+	create_matrix(NewRows, Columns, NewRow),
+	append([InsideRow], NewRow, Matrix).
 
-restrict_equal_wheel(Wheel, OuterWheel, Index, Size) :-
-	nth0(Index, Wheel, WheelElem),
-	nth0(Index, OuterWheel, OuterWheelElem),
-	OuterWheelElem #= WheelElem,
-	NextIndex is Index + 1,
-	restrict_equal_wheel(Wheel, OuterWheel, NextIndex, Size).
+apply_restriction_by_row([]).
+apply_restriction_by_row([H|T]) :-
+	domain(H, 1, 8),
+	apply_restriction_by_row(T).
 
-get_outer_wheel(OuterWheel) :-
-	outerWheel(Wheel),
-	length(Wheel, Size),
-	length(OuterWheel, Size),
-	S is Size - 2,
-	restrict_equal_wheel(Wheel, OuterWheel, 0, S).
+apply_restriction_by_column([], _).
+apply_restriction_by_column([H|T], Sum) :-
+	sum(H, #=, Sum),
+	apply_restriction_by_column(T, Sum).
 
-get_existing_wheels(ExistingWheels) :-
-	findall(List, innerWheel(List), ExistingWheels).
+generate_random_solution(Rows, Columns, Sum) :-
+	Temp is Rows * Columns * Sum,
+	random(1, Temp, RandomValue),
+	retract(randomSolution(_)),
+	assert(randomSolution(RandomValue)).
 
-create_wheel_list(WheelList) :-
-	innerWheel(List),
-	length(List, Size),
-	length(WheelList, Size).
-
-create_inner_wheels_list([], 0).
-create_inner_wheels_list(InnerWheelsList, N) :-
-	create_wheel_list(WheelList),
-	domain(WheelList, 1, 8),
-	N1 is N - 1,
-	create_inner_wheels_list(NewWheelList, N1),
-	append([WheelList], NewWheelList, InnerWheelsList).
-
-count_value_in_list(List, Value, NumberOfReps) :-
-	findall(Value, member(Value, List), L),
-	length(L, NumberOfReps).
-
-create_cardinality(_, [], 10).
-create_cardinality(ExistingWheel, CardinalityList, N) :-
-	N1 is N + 1,
-	count_value_in_list(ExistingWheel, N, NumberOfReps),
-	create_cardinality(ExistingWheel, NewCardinalityList, N1),
-	append([N - NumberOfReps], NewCardinalityList, CardinalityList).
-
-list_valid(ExistingWheels, InnerWheelsList, Index) :-
-	nth1(Index, ExistingWheels, ExistingWheel),
-	nth1(Index, InnerWheelsList, InnerWheelList),
-	create_cardinality(ExistingWheel, CardinalityList, 1),
-	global_cardinality(InnerWheelList, CardinalityList).
-
-restrict_valid_lists(ExistingWheels, InnerWheelsList) :-
-	length(ExistingWheels, Size),
-	foreach(between(1, Size, Index), list_valid(ExistingWheels, InnerWheelsList, Index)).
-
-label_inner_wheels([]).
-label_inner_wheels([H|T]) :-
-	labeling([], H),
-	label_inner_wheels(T).
-
-create_list_by_index([], _, []).
-create_list_by_index([H|T], Index, List) :-
-	nth1(Index, H, Elem),
-	create_list_by_index(T, Index, NewList),
-	append([Elem], NewList, List).
-
-restrict_sum_column(InnerWheelsList, Index) :-
-	create_list_by_index(InnerWheelsList, Index, Column),
-	sum(Column, #=, 15).
-
-restrict_sum(InnerWheelsList) :-
-	innerWheel(List),
-	length(List, Size),
-	foreach(between(1, Size, Index), restrict_sum_column(InnerWheelsList, Index)).
-
-restrict_sum_column(OuterWheel, InnerWheelsList, Index) :-
-	nth1(Index, OuterWheel, First),
-	create_list_by_index(InnerWheelsList, Index, NewList),
-	append([First], NewList, Column),
-	sum(Column, #=, 20).
-
-restrict_sum(OuterWheel, InnerWheelsList) :-
-	length(OuterWheel, Size),
-	foreach(between(1, Size, Index), restrict_sum_column(OuterWheel, InnerWheelsList, Index)).
-
-% Main function
-main :-
-	choose_difficulty(Input),
-	get_existing_wheels(ExistingWheels),
-	length(ExistingWheels, Size),
-	create_inner_wheels_list(InnerWheelsList, Size),
-	restrict_valid_lists(ExistingWheels, InnerWheelsList),
-	(Input = e -> restrict_sum(InnerWheelsList),
-				  label_inner_wheels(InnerWheelsList),
-				  display_inner_wheels(InnerWheelsList), !;
-	(Input = h -> get_outer_wheel(OuterWheel),
-				  restrict_sum(OuterWheel, InnerWheelsList),
-				  label_inner_wheels(InnerWheelsList),
-				  append([OuterWheel], InnerWheelsList, AllWheelsList),
-				  display_all_wheels(AllWheelsList), !)),
-	write('The End!').
+% Starting function
+main(Rows, Columns, Sum) :-
+	create_matrix(Rows, Columns, Matrix),
+	transpose(Matrix, TransposedMatrix),
+	apply_restriction_by_row(Matrix),
+	apply_restriction_by_column(TransposedMatrix, Sum),
+	generate_random_solution(Rows, Columns, Sum),
+	append(Matrix, List),
+	reset_timer,
+	labeling([], List),
+	randomSolution(V),
+	(V = 0 -> display_board(Matrix);
+	(V \= 0 -> randomSolution(N),
+			   N1 is N - 1,
+			   retract(randomSolution(_)),
+			   assert(randomSolution(N1)),
+			   fail)),
+	print_time,
+	fd_statistics.
