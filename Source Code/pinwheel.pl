4073a385292abfcad198d75373775a2edc9ef2d1
Mosaal <alexandremoreira_13@sapo.pt>
2015-12-11 17:22:59 +0000

    Falta o relatorio

--- Source Code/pinwheel.pl
+++ Source Code/pinwheel.pl
@@ -1,23 +1,49 @@
 :- use_module(library(lists)).
 :- use_module(library(clpfd)).
-:- use_module(library(random)).
+:- use_module(library(samsort)).
 
-% Aux. Variables
-:- dynamic randomSolution/1.
-randomSolution(0).
+% Boards
+smallMatrix([[5, 5, 4, 6, 4, 6, 6, 4, 5],
+			 [7, 5, 7, 8, 4, 5, 3, 2, 6],
+			 [4, 8, 5, 6, 2, 1, 4, 6, 7]]).
+
+largeMatrix([[3, 3, 4, 5, 7, 4, 8, 6, 5],
+			 [5, 5, 4, 6, 4, 6, 6, 4, 5],
+			 [7, 5, 7, 8, 4, 5, 3, 2, 6],
+			 [4, 8, 5, 6, 2, 1, 4, 6, 7]]).
 
 % Interface
 display_row([]).
 display_row([H|T]) :-
-	(H < 10 -> format('|  ~w  ', [H]), !;
-	(H > 9, H < 99 -> format('| ~w  ', [H]), !;
-	(H > 99, H < 999 -> format('| ~w ', [H]), !))),
+	format('| ~w ', [H]),
 	display_row(T).
 
-display_board([]).
-display_board([H|T]) :-
+display_matrix([]).
+display_matrix([H|T]) :-
 	display_row(H), write('|'), nl,
-	display_board(T).
+	display_matrix(T).
+
+display_board(Matrix) :-
+	write('+-----------------------------------+'), nl,
+	display_matrix(Matrix),
+	write('+-----------------------------------+'), nl.
+
+display_info(Input) :-
+	(Input = e -> write('The sum of each column is 15!');
+	(Input = h -> write('The sum of each column is 20!'))).
+
+valid_input(Input) :-
+	Input = e, !;
+	Input = h, !;
+	fail.
+
+choose_difficulty(Input) :-
+	write('Choose difficulty:'), nl,
+	write(' - Easy (e)'), nl,
+	write(' - Hard (h)'), nl,
+	read(Input), nl,
+	valid_input(Input), !;
+	choose_difficulty(Input).
 
 reset_timer :- statistics(walltime, _).
 print_time :-
@@ -26,50 +52,51 @@ print_time :-
 	nl, format('Time: ~ws', [TS]), nl.
 
 % Logic
-create_matrix(0, _, _).
-create_matrix(Rows, Columns, Matrix) :-
-	NewRows is Rows - 1,
-	length(InsideRow, Columns),
-	create_matrix(NewRows, Columns, NewRow),
-	append([InsideRow], NewRow, Matrix).
+get_small_matrix(Matrix) :-
+	smallMatrix(Matrix).
 
-apply_restriction_by_row([], _).
-apply_restriction_by_row([H|T], Sum) :-
-	domain(H, 1, Sum),
-	apply_restriction_by_row(T, Sum).
+get_large_matrix(Matrix) :-
+	largeMatrix(Matrix).
 
-apply_restriction_by_column([], _).
-apply_restriction_by_column([H|T], Sum) :-
+get_matrix(Input, Matrix) :-
+	(Input = e -> get_small_matrix(Matrix);
+	(Input = h -> get_large_matrix(Matrix))).
+
+label_matrix([]).
+label_matrix([Row | Rest]) :-
+	labeling([], Row),
+	label_matrix(Rest).
+
+apply_restriction([], _).
+apply_restriction([H|T], Sum) :-
 	sum(H, #=, Sum),
-	apply_restriction_by_column(T, Sum).
+	apply_restriction(T, Sum).
 
-generate_random_solution(Rows, Columns, Sum) :-
-	Temp is Rows * Columns * Sum,
-	% Temp is Rows * Columns * Sum,
-	random(1, Temp, RandomValue),
-	retract(randomSolution(_)),
-	assert(randomSolution(RandomValue)).
+order_matrix([], []).
+order_matrix([Row | Rest], [OrderedRow | OrderedRest]) :-
+	samsort(Row, OrderedRow),
+	order_matrix(Rest, OrderedRest).
+
+sort_matrix([], []).
+sort_matrix([Row | Rest], [SortedRow | SortedRest]) :-
+	length(Row, Size),
+	length(SortedRow, Size),
+	length(Temp, Size),
+	sorting(SortedRow, Temp, Row),
+	sort_matrix(Rest, SortedRest).
+
+solve_puzzle(Input, Matrix, FinalMatrix) :-
+	order_matrix(Matrix, OrderedMatrix),
+	sort_matrix(OrderedMatrix, FinalMatrix),
+	transpose(FinalMatrix, TransposedMatrix),
+	(Input = e -> apply_restriction(TransposedMatrix, 15);
+	(Input = h -> apply_restriction(TransposedMatrix, 20))).
 
 % Starting function
-main(Rows, Columns, Sum, List) :-
-	(Sum < Rows -> write('The Sum must be greater or equal to the number of Rows!'), fail);
-	% Max is Sum - (Rows - 1),
-	create_matrix(Rows, Columns, Matrix),
-	transpose(Matrix, TransposedMatrix),
-	apply_restriction_by_row(Matrix, Sum),
-	append(Matrix, List),
-	% random_permutation(List, RandomList),
-	apply_restriction_by_column(TransposedMatrix, Sum),
-	% generate_random_solution(Rows, Columns, Sum),
-	reset_timer,
-	labeling([], List),
-	% randomSolution(V),
-	% (Sum = Rows -> display_board(Matrix);
-	% (V = 0 -> display_board(Matrix);
-	% (V \= 0 -> randomSolution(N), N1 is N - 1,
-	%		   retract(randomSolution(_)),
-	%		   assert(randomSolution(N1)),
-	%		   fail))),
-	display_board(Matrix),
-	print_time,
-	fd_statistics.
+main :-
+	choose_difficulty(Input),
+	get_matrix(Input, Matrix),
+	solve_puzzle(Input, Matrix, FinalMatrix),
+	label_matrix(FinalMatrix),
+	display_board(FinalMatrix),
+	display_info(Input).
